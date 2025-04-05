c379b9ae9317899f72ba6e6d8183c7e68f86780b
Krish-006-19 <krish.ramena24@vit.edu>
2025-04-05 13:47:56 +0530

    Update useAddTransaction.js

--- src/hooks/useAddTransaction.js
+++ src/hooks/useAddTransaction.js
@@ -3,18 +3,6 @@ import { database } from "../config/firebase-config"
 import {useInfo} from './useInfo'
 export let useAddTransaction = ()=>{
     let transcollection = collection(database,'transactions')
-
-/*
-
-    let obj = useInfo()
-    let userID = obj.userID
-
-OR
-
-    let { userID } = useInfo()
-
-*/
-
     let { userID } = useInfo()
     let addTransaction=async({description, transAmount, transType,})=>{
         await addDoc(transcollection,{
